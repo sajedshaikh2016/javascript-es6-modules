d9732dce1414c948a234e19eeaaa29c8bbf677a2
Sajed Shaikh <sajed.shaikh@hotmail.com>
2022-10-10 14:56:18 +0530

    JavaScript module

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@
 
 
 import * as Guitars from "./guitars.js"
+import User from "./user.js";
+
+const me = new User("sajed@email.com", "Sajed");
+console.log(me);
+console.log(me.greetings());
+
 console.log(Guitars.default());
 console.log(Guitars.shredding());
 console.log(Guitars.plucking());
